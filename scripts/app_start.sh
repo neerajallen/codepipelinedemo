b0fc2d580af16b9220dc32892b260b6f72a06d9e
neerajallen <100480612+neerajallen@users.noreply.github.com>
2023-01-27 11:28:56 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -6,5 +6,5 @@ cd /home/ubuntu/server
 #_Delete_Old_PM2_Service
 #sudo pm2 delete Frontend
 #sudo pm2 start server.js --name Frontend
-pm2 delete Frontend
+#pm2 delete Frontend
 pm2 start server.js --name Frontend
